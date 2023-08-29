086db5f80dceed57c5749b7affab480cde318718
Aaron <20589499+aaronlefkow@users.noreply.github.com>
2023-08-29 17:36:37 -0500

    Fix linkes with texts after ".pdf" not working correctly.

--- script.js
+++ script.js
@@ -6,11 +6,8 @@ function modifySharePointLink(link) {
         
         // Remove text including and after "&parent"
         const modifiedLink = step1.replace(/&parent.*/, "");
-
-        // Remove text after ".pdf"
-        const finalLink = modifiedLink.replace(/\.pdf.*/, ".pdf");
         
-        return decodeURIComponent(finalLink);
+        return decodeURIComponent(modifiedLink);
     }
     
     // Scenario 2: If the link has "Forms/AllItems.aspx?id="
@@ -20,11 +17,8 @@ function modifySharePointLink(link) {
         
         // Remove text including and after "&parent"
         const modifiedLink = step1.replace(/&parent.*/, "");
-
-        // Remove text after ".pdf"
-        const finalLink = modifiedLink.replace(/\.pdf.*/, ".pdf");
         
-        return decodeURIComponent(finalLink);
+        return decodeURIComponent(modifiedLink);
     }
     
     // Return unmodified link if none of the scenarios match
@@ -32,7 +26,13 @@ function modifySharePointLink(link) {
 }
 
 function replaceSpacesWithPercent20(link) {
-    return link.replace(/\s/g, '%20');
+    // Replace spaces with %20
+    link = link.replace(/\s/g, '%20');
+
+    // Truncate everything after .pdf
+    link = link.replace(/\.pdf.*/, '.pdf');
+
+    return link;
 }
 
 function modifyUrl() {
@@ -48,24 +48,31 @@ function modifyUrl() {
 
 function openModifiedUrl() {
     const inputUrlElement = document.getElementById("inputUrl");
+    const outputUrlElement = document.getElementById("outputUrl");
+
     const inputUrl = inputUrlElement.value;
     const modifiedUrl = modifySharePointLink(inputUrl);
     const encodedUrl = replaceSpacesWithPercent20(modifiedUrl);
+    
+    outputUrlElement.textContent = encodedUrl;
 
     // Open the modified URL in a new tab
     window.open(encodedUrl, "_blank");
 }
 
 // Test scenarios
-// Test scenarios
-// const scenario1Input = "https://allinahealth1com.sharepoint.com/teams/ReferralManagement/Shared%20Documents/Forms/AllItems.aspx?FolderCTID=0x01200076B57FF83E2A5940B3487FCBFEB75B23&id=%2Fteams%2FReferralManagement%2FShared%20Documents%2FGeneral%2FWorkflows%2FPrior%20Authorizations%20Using%20Payer%20Websites%20%2D%20Resource%20Guide%2Epdf&parent=%2Fteams%2FReferralManagement%2FShared%20Documents%2FGeneral%2FWorkflows";
-// const scenario2Input = "https://allinahealth1com.sharepoint.com/sites/rcm-RCMpriorAuth/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2Frcm%2DRCMpriorAuth%2FShared%20Documents%2FCollaboration%2FCPA%20%2D%20Patient%20WQ%20Workflow%2Epdf&parent=%2Fsites%2Frcm%2DRCMpriorAuth%2FShared%20Documents%2FCollaboration";
+const scenario1Input = "https://allinahealth1com.sharepoint.com/teams/ReferralManagement/Shared%20Documents/Forms/AllItems.aspx?FolderCTID=0x01200076B57FF83E2A5940B3487FCBFEB75B23&id=%2Fteams%2FReferralManagement%2FShared%20Documents%2FGeneral%2FWorkflows%2FPrior%20Authorizations%20Using%20Payer%20Websites%20%2D%20Resource%20Guide%2Epdf&parent=%2Fteams%2FReferralManagement%2FShared%20Documents%2FGeneral%2FWorkflows";
+const scenario2Input = "https://allinahealth1com.sharepoint.com/sites/rcm-RCMpriorAuth/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2Frcm%2DRCMpriorAuth%2FShared%20Documents%2FCollaboration%2FCPA%20%2D%20Patient%20WQ%20Workflow%2Epdf&parent=%2Fsites%2Frcm%2DRCMpriorAuth%2FShared%20Documents%2FCollaboration";
+const scenario3Input = "https://allinahealth1com.sharepoint.com/sites/rcm-RCMpriorAuth/Shared%20Documents/Forms/AllItems.aspx?csf=1&web=1&e=ulCIXs&FolderCTID=0x012000B17DE35911DFBD4895961977FBA1A94A&noAuthRedirect=1&id=%2Fsites%2Frcm%2DRCMpriorAuth%2FShared%20Documents%2FHospital%2FWorkflows%2FCPA%20%2D%20Hospital%20Notification%20PANs%2Epdf&viewid=09e91ede%2Db587%2D4497%2Db05a%2D6ffe12016e77&parent=%2Fsites%2Frcm%2DRCMpriorAuth%2FShared%20Documents%2FHospital%2FWorkflows"
 
-// const scenario1Output = modifySharePointLink(scenario1Input);
-// const scenario2Output = modifySharePointLink(scenario2Input);
+const scenario1Output = modifySharePointLink(scenario1Input);
+const scenario2Output = modifySharePointLink(scenario2Input);
+const scenario3Output = modifySharePointLink(scenario3Input);
 
-// const scenario1Final = replaceSpacesWithPercent20(scenario1Output);
-// const scenario2Final = replaceSpacesWithPercent20(scenario2Output);
+const scenario1Final = replaceSpacesWithPercent20(scenario1Output);
+const scenario2Final = replaceSpacesWithPercent20(scenario2Output);
+const scenario3Final = replaceSpacesWithPercent20(scenario3Output);
 
-// // console.log("Scenario 1 final output:", scenario1Final);
-// // console.log("Scenario 2 final output:", scenario2Final);
+console.log("Scenario 1 final output:", scenario1Final);
+console.log("Scenario 2 final output:", scenario2Final);
+console.log("Scenario 3 final output:", scenario3Final);
